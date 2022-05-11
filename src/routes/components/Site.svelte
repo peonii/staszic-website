e5d9c1f7dcad63558097bae1306dab649bb46221
RealQuerter <54771118+RealQuerter@users.noreply.github.com>
2022-05-11 22:10:28 +0200

    asdfj

--- src/routes/components/Site.svelte
+++ src/routes/components/Site.svelte
@@ -4,7 +4,7 @@
     export let desc;
 </script>
 
-<a href={route} class="w-1/2">
+<a href={route} class="lg:w-1/2 2xl:w-1/3 w-full">
     <div class="rounded-2xl mx-12 p-4 my-6 min-w-1/3 border-slate-100 border-2 hover:shadow-lg hover:border-white hover:scale-105 transition">
             <h1 class="text-xl font-bold text-red-500">{name}</h1>
             <p class="text-slate-700">{desc}</p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -22,6 +22,6 @@
     {/each}
 </div>
 
-<div class="select-none cursor-default text-slate-200 text-4xl text-center my-32 animate-pulse">
+<div class="select-none cursor-default text-slate-200 text-2xl md:text-3xl lg:text-4xl text-center my-32 animate-pulse">
     More sites coming soon...
 </div>
